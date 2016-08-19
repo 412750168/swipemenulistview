f3dca2b6a5bf482871a9e2634299c3f0017e0588
zilong zhang <joyo456@126.com>
2016-08-19 12:17:19 +0800

    Update MainActivity.java

--- demo/src/main/java/com/baoyz/swipemenulistview/demo/MainActivity.java
+++ demo/src/main/java/com/baoyz/swipemenulistview/demo/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //add to test git 
+        //add this data int master/remote 
 
     }
 

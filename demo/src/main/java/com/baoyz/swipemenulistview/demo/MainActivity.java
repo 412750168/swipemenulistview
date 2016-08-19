9cbf8d65609b7ea1a12083eead0176439a57650a
zilong zhang <joyo456@126.com>
2016-08-19 12:23:40 +0800

    Update MainActivity.java

--- demo/src/main/java/com/baoyz/swipemenulistview/demo/MainActivity.java
+++ demo/src/main/java/com/baoyz/swipemenulistview/demo/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends Activity {
         //add to test git 
         //add this data int master/remote 
         //add data remote/second
-
+        //add data remote/third
     }
 
     public void onClick(View v){
